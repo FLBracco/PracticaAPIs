5c6534b870ef3b8775912be1ff064f5823d73063
Fabricio Bracco <Braccof.0601@gmail.com>
2024-11-28 00:58:46 -0300

    Feature: se agrego metodo post, falta validar, realizar manejo con try/catch.

--- Nivel1/To-Do-List/app.js
+++ Nivel1/To-Do-List/app.js
@@ -6,11 +6,10 @@ import express from 'express'
 const app = express();
 app.disable('x-powered-by');
 app.use(express.json());
-
 const disaredPort = process.env.PORT || 3000;
 
 app.listen(disaredPort, ()=>{
-    console.log(`App listen to PORT: ${disaredPort}`);
+    console.log(`App listen on PORT: http://localhost:${disaredPort}`);
 })
 
 const list = [];
@@ -18,4 +17,18 @@ const list = [];
 // Mostrar todas las tareas
 app.get('/todo', (req, res) => {
     res.status(200).send(list);
+})
+
+app.post('/todo', (req, res) => {
+    const { title, description, state } = req.body;
+    list.push(
+        {
+            id: list.length + 1,
+            titulo: title,
+            descripcion: description,
+            estado: state
+        }
+    );
+    res.status(200).send({message: 'Se creo la tarea correctamente.'})
+    console.log(list);
 })
